3ffd1169742c3c977f1662ed9d941e52d9ef0979
Musatov Yuriy <colorkid@yandex.ru>
2018-02-06 06:44:16 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,59 +1,62 @@
 onmessage = function(e) {
   const numberOfIterationForStatistic = 1000;
-    
-  try { 
+
+  let snippetFirst = e.data[0].snippetOne;
+  let snippetSecond = e.data[0].snippetTwo;
+
+    try { 
+
+      if (snippetFirst.length === 0 || snippetSecond.length === 0) {
+        throw new Error("Заполните поля");
+        postMessage(["error", err.name + "\n" + err.message]);
+      }
           
-    if (e.data[0].length === 0 || e.data[1].length === 0) {
-      throw new Error("Заполните поля");
-      postMessage(["error", err.name + "\n" + err.message + "\n" + err.stack]);
-    }
+      let timeOneIteration;
+      let iterationIndex = 0;
 
-    let timeOneIteration;
-    let iterationIndex = 0;
+      function timing(code) {
+        let time = performance.now();
+        let evl = eval.bind();
+        evl(code);
+        time = performance.now() - time;
+        timeOneIteration = time.toFixed(5);
+      };
 
-    function timing(code) {
-      let time = performance.now();
-      let evl = eval.bind();
-      evl(code);
-      time = performance.now() - time;
-      timeOneIteration = time.toFixed(5);
-    };
+      function averageTimeCalculation(code) {
+        let arr = [];
 
-    function averageTimeCalculation(code) {
-      let arr = [];
+        for(let i = 0; i < numberOfIterationForStatistic; i++){
 
-      for(let i = 0; i < numberOfIterationForStatistic; i++){
+          timing(code);
+          arr.push(+timeOneIteration); // + because timeOneIteration is type String
+          // 20 - is the number by which we divide the total number of iterations, (two snippets over numberOfIterationForStatistic iterations) to get 100.
+          if (i % 20 === 0) {
+            iterationIndex++;
+            postMessage([iterationIndex]);
+          }
 
-        timing(code);
-        arr.push(+timeOneIteration); // + because timeOneIteration is type String
-        // 20 - is the number by which we divide the total number of iterations, (two snippets over numberOfIterationForStatistic iterations) to get 100.
-        if (i % 20 === 0) {
-          iterationIndex++;
-          postMessage([iterationIndex]);
         }
 
+        let result = arr.reduce((sum, current) => {
+          return sum + current;
+        });
+
+        return result / arr.length;
+      };
+
+      let runTimeFirstSnippet = averageTimeCalculation(snippetFirst);
+      let runTimeSecondSnippet = averageTimeCalculation(snippetSecond);
+
+      if(runTimeFirstSnippet < runTimeSecondSnippet) {
+        postMessage([[runTimeFirstSnippet, snippetFirst], [(runTimeSecondSnippet - runTimeFirstSnippet).toFixed(3)]]);
       }
+      
+      else {
+        postMessage([[runTimeSecondSnippet, snippetSecond], [(runTimeFirstSnippet - runTimeSecondSnippet).toFixed(3)]]);
+      }
+    } 
 
-      let result = arr.reduce(function(sum, current) {
-        return sum + current;
-      });
-              
-      return result / arr.length;
-    };
-
-    let runTimeFirstSnippet = averageTimeCalculation(e.data[0]);
-    let runTimeSecondSnippet = averageTimeCalculation(e.data[1]);
-
-    if(runTimeFirstSnippet < runTimeSecondSnippet) {
-      postMessage([[runTimeFirstSnippet, e.data[0]], [(runTimeSecondSnippet - runTimeFirstSnippet).toFixed(3)]]);
+    catch (err){
+      postMessage(["error", err.name + "\n" + err.message]);
     }
-    
-    else {
-      postMessage([[runTimeSecondSnippet, e.data[1]], [(runTimeFirstSnippet - runTimeSecondSnippet).toFixed(3)]]);
-    }
-  } 
-
-  catch (err){
-    postMessage(["error", err.name + "\n" + err.message]);
-  }
 };
