171530bbdb365380eb7217b982fb362fe0d33d09
Musatov Yuriy <colorkid@yandex.ru>
2018-02-07 06:40:24 +0300

    Update compare.js

--- compare.js
+++ compare.js
@@ -16,21 +16,8 @@ export default class Compare {
     this.worker.postMessage([objForPostMessage]);
 
     this.worker.onmessage = function(e) {
-    	console.log(e.data)
-      //get the data for bar progress
-      if(e.data.length === 1) {
-        this.progressIndex = e.data[0];
-      }
-      //obtain the result of calculations
-      else {
-        this.fastestTime = e.data[0][0];
-        this.fastestCode = e.data[0][1];
-        this.differenceSpeed = e.data[1][0];
-        this.errorMessage = e.data[1];
-        this.compareButtonDisabled = false;
-      }
-
-      fromModelToView(this.progressIndex, this.compareButtonDisabled, this.fastestTime, this.fastestCode, this.differenceSpeed, this.errorMessage);
+	    let objFromWorker = e.data[0];
+      fromModelToView(objFromWorker);
     };
   }
 };
